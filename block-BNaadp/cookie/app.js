325f8e06dab92c7fd6fe20635b7f5e07400d57b4
Gurinder Khabra <gurinderkhabra@gmail.com>
2022-05-04 11:11:32 -0700

    Done

---
+++ block-BNaadp/cookie/app.js
@@ -0,0 +1,64 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// // set a cookie
+app.get('/user/:user', function (req, res) {
+  res.cookie('name', req.params.user).send('<p>Cookie Set: <a href="/user">View Here</a></p>');
+});
+app.get('/user', function (req, res) {
+  res.send(req.cookies.name);
+});
+// app.use(function (req, res, next) {
+//   // check if client sent cookie
+//   var cookie = req.cookies.cookieName;
+//   if (cookie === undefined) {
+//     // no: set a new cookie
+//     var randomNumber = Math.random().toString();
+//     randomNumber = randomNumber.substring(2, randomNumber.length);
+//     res.cookie('cookieName', randomNumber, { maxAge: 900000, httpOnly: true });
+//     console.log('cookie created successfully');
+//   } else {
+//     // yes, cookie was already present
+//     console.log('cookie exists', cookie);
+//   }
+//   next(); // <-- important!
+// });
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
